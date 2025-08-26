4bbd84177e4bb8816e7a12d5113429bc9db1c09c
Kalpesh Khule <kalpesh.khule@assurant.com>
2025-08-26 15:49:36 +0530

    change the basepath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Enables static HTML export
-  basePath: "/website", // Set this to your repository name
   images: {
     unoptimized: true, // Disables Next.js image optimization
   },
